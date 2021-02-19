4c45dc171f7c6475d974935a6ccb568aeacfc626
asma20 <jbeli.asma20@gmail.com>
2021-02-19 12:22:07 +0100

    1 essai

---
+++ Partie4Exercice5TABEmb/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Partie4Exercice5TABEmb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] tab= new string[,] { { "one", "two" }, { "three", "four" },
+                                        { "five", "six" } };
+            // System.Console.Write(fujita[0, 0]+";");
+            // System.Console.Write(fujita[0,1]);
+            // Console.WriteLine();
+
+
+            /*  for (int i = 0; i <3; i++) {
+
+                  for (int j = i; j <i; j++) {
+
+
+
+        Console.Write(fujita[i, j] + ";" );
+                    Console.Write(fujita[i, j]  );
+
+
+                    Console.WriteLine();
+
+
+
+
+                }
+              }
+
+           */
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+
+
+
+
+                    if (j == 1)
+                    {
+                        Console.WriteLine(tab[i, j]);
+                    }
+
+                    else
+                    {
+                        Console.Write(tab[i, j]);
+                    }
+
+                }
+            }
+
+
+
+
+        }
+    }
+}
